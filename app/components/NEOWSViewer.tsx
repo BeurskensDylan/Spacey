e13d7a780c9a3da359c4ffad9274c660c6657b11
Deebzie <deebzieonline@gmail.com>
2026-03-26 10:18:18 +0100

    Fix spacing and alignment of the NEOWS viewer

--- app/components/NEOWSViewer.tsx
+++ app/components/NEOWSViewer.tsx
@@ -35,30 +35,32 @@ export default function NEOWSViewer({ apiUrl }: NEOWSViewerProps) {
     }
 
     return (
-        <div className="flex sm:flex-col lg:flex-row justify-center items-center w-full">
+        <div className="flex flex-col justify-center items-center w-full">
             <button className="border border-white p-2 rounded hover:bg-white hover:text-black transition-all duration-300" onClick={fetchData}>
                 Get a list of near earth objects
             </button>
-            {asteroids.map((asteroid) => (
-                <div key={asteroid.id} className="bg-black/60 rounded-2xl backdrop-blur-sm flex flex-col justify-center items-center w-100 m-4 p-4 gap-2">
-                    <h2 className="text-2xl font-bold text-center">{asteroid.name}</h2>
-                    <p className="text-center">
-                        📅 {asteroid.close_approach_data[0].close_approach_date_full}
-                    </p>
-                    <p className="text-center">
-                        💨 {Number(asteroid.close_approach_data[0].relative_velocity.kilometers_per_hour).toFixed(0)} km/h
-                    </p>
-                    <p className="text-center">
-                        📏 {asteroid.estimated_diameter.kilometers.estimated_diameter_max.toFixed(4)} km (max diameter)
-                    </p>
-                    <p className="text-center">
-                        🌍 Miss distance: {Number(asteroid.close_approach_data[0].miss_distance.kilometers).toLocaleString()} km
-                    </p>
-                    <p className={asteroid.is_potentially_hazardous_asteroid ? "text-red-500 font-bold" : "text-green-400"}>
-                        {asteroid.is_potentially_hazardous_asteroid ? "⚠️ Potentially Hazardous" : "✅ Not Hazardous"}
-                    </p>
-                </div>
-            ))}
+            <div className="flex flex-col lg:flex-row justify-center items-center w-full">
+                {asteroids.map((asteroid) => (
+                    <div key={asteroid.id} className="bg-black/60 rounded-2xl backdrop-blur-sm flex flex-col justify-center items-center w-100 m-4 p-4 gap-2">
+                        <h2 className="text-2xl font-bold text-center">{asteroid.name}</h2>
+                        <p className="text-center">
+                            📅 {asteroid.close_approach_data[0].close_approach_date_full}
+                        </p>
+                        <p className="text-center">
+                            💨 {Number(asteroid.close_approach_data[0].relative_velocity.kilometers_per_hour).toFixed(0)} km/h
+                        </p>
+                        <p className="text-center">
+                            📏 {asteroid.estimated_diameter.kilometers.estimated_diameter_max.toFixed(4)} km (max diameter)
+                        </p>
+                        <p className="text-center">
+                            🌍 Miss distance: {Number(asteroid.close_approach_data[0].miss_distance.kilometers).toLocaleString()} km
+                        </p>
+                        <p className={asteroid.is_potentially_hazardous_asteroid ? "text-red-500 font-bold" : "text-green-400"}>
+                            {asteroid.is_potentially_hazardous_asteroid ? "⚠️ Potentially Hazardous" : "✅ Not Hazardous"}
+                        </p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
